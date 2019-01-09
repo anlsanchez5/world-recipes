4f347c0d49818f43e7c82f1b2586afaaf8891531
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-01-09 21:21:40 +0000

    object relationships

--- lib/world_recipes/cuisine.rb
+++ lib/world_recipes/cuisine.rb
@@ -1,10 +1,15 @@
 class WorldRecipes::Cuisine
   attr_accessor :name, :url, :food_categories
-
-  def initialize
+  @@all = []
+  def initialize(name, url, food_categories)
+    @name = name
+    @url = url
+    @food_categories = food_categories
+    @@all << self
   end
 
   def self.all
+    @@all
     cuisine_1 = self.new
     #cuisine_1.food_categories = WorldRecipes::FoodCategory.all # this will be an array of FoodCagegory instances
     cuisine_1.name = "Mexican"
@@ -18,11 +23,16 @@ class WorldRecipes::Cuisine
     [cuisine_1, cuisine_2]
   end
 
+  def new_category(name, url, recipes)
+    WorldRecipes::FoodCategory.new(name,url, recipes, self)
+  end
+
   def list_category
     @food_categories = WorldRecipes::FoodCategory.all
     @food_categories.each.with_index(1) do |category, i|
       puts "#{i}. #{category.name}"
     end
   end
+
 
 end
--- lib/world_recipes/food_category.rb
+++ lib/world_recipes/food_category.rb
@@ -1,21 +1,29 @@
 class WorldRecipes::FoodCategory
     attr_accessor :name, :url, :recipes
+    @@all = []
 
-    def initialize
+    def initialize(name, url)
+      @name = name
+      @url = url
+      @@all << self
     end
 
     def self.all
-      category_1 = self.new
-      category_1.name = "Desserts"
-      category_1.recipes = "list of desserts " # this will be an array of Recipe instances
-      category_1.url = "http://allrecipes.com/mexican"
+      @@all
+    #  category_1 = self.new
+    #  category_1.name = "Desserts"
+    #  category_1.recipes = "list of desserts " # this will be an array of Recipe instances
+    #  category_1.url = "http://allrecipes.com/mexican"
+    #  category_2 = self.new
+    #  category_2.name = "Appetizers"
+    #  category_2.recipes = "list of Appetizers" # this will be an array of Recipe instances
+    #  category_2.url = "http://allrecipes.com/indian"
+    #  [category_1, category_2]
+    end
 
-      category_2 = self.new
-      category_2.name = "Appetizers"
-      category_2.recipes = "list of Appetizers" # this will be an array of Recipe instances
-      category_2.url = "http://allrecipes.com/indian"
-
-      [category_1, category_2]
+    def add_recipe(recipe)
+      @recipes << recipe
+      recipe.food_category = self unless recipe.food_category == self
     end
 
     def list_recipes
