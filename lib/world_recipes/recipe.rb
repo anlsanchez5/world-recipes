92ac8177a3f05bec98700ab3893f3bdf49abcf56
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-01-14 19:53:15 +0000

    display recipe

--- lib/world_recipes/recipe.rb
+++ lib/world_recipes/recipe.rb
@@ -20,26 +20,47 @@ class WorldRecipes::Recipe
 
  def get_recipe_data
     doc = WorldRecipes::Scraper.new.scrape_recipe_data(self.url)
-
-    #self.number_servings = doc.css("")
+    self.number_servings = doc.css("meta#metaRecipeServings").attribute("content").text + " Servings"
     self.cooking_time = doc.css("span.ready-in-time").text
-    self.ingredients = doc.css("li span.recipe-ingred_txt").text
+    self.ingredients = []
+    ingredients = doc.css("li span.recipe-ingred_txt")
+    ingredients.each do |i|
+      self.ingredients << i.text
+    end
+    self.ingredients.delete_if{|x| x >= "Add all ingredients to list"}
     self.instructions = doc.css("span.recipe-directions__list--item").text
+  end
+
+  def display_ingredients
+    @ingredients.each do |i|
+      puts "#{
+      i}"
+    end
+  end
+
+  def display_instructions
+    @instructions = @instructions.split("\n")
+  #  binding.pry
+    @instructions.each do |i|
+      puts "#{i}"
+    end
   end
 
   def display_recipe
     get_recipe_data
+  #  binding.pry
     puts <<-DOC.gsub /^\s*/, ''
+
       #{@name}
       Link for Picture: #{@picture_url}
       _____________________
-      #{@number_servings}|#{@cooking_time}
-      _____________________
+      #{@number_servings} | #{@cooking_time}
+      ---------------------
+    DOC
 
-      #{@ingredients}
-
-      Instructions
-       #{@instructions}
-     DOC
+    puts "Ingredients"
+    display_ingredients
+    puts "Instructions"
+    display_instructions
   end
 end
