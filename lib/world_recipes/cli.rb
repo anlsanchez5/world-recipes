fe12a9ed184430d356e2ec61705261dd7580599f
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-01-16 23:28:02 +0000

    back option for recipe

--- lib/world_recipes/cli.rb
+++ lib/world_recipes/cli.rb
@@ -31,6 +31,7 @@ class WorldRecipes::CLI
     puts "Enter the number of the cuisine you'd like more food categories on or type exit:"
     @input << gets.strip.downcase
     if @input.last.to_i > 0 && @input.last.to_i <= @cuisines.length.to_i
+#        binding.pry
       @cuisines[@input.last.to_i-1].list_categories
     elsif @input.last == "exit"
       goodbye
@@ -49,10 +50,10 @@ class WorldRecipes::CLI
          type back to see cuisine list again or type exit:"
     @input << gets.strip.downcase
     i = @input[(@input.length) - 2]
-    @food_categories = @cuisines[i.to_i-1].food_categories
-#    binding.pry
-    if @input.last.to_i > 0 && @input.last.to_i <= @food_categories.length.to_i
-      @food_categories[@input.last.to_i-1].list_recipes
+    food_categories = @cuisines[i.to_i-1].food_categories
+  #  binding.pry
+    if @input.last.to_i > 0 && @input.last.to_i <= food_categories.length.to_i
+      food_categories[@input.last.to_i-1].list_recipes
     elsif @input.last == "back"
       start
     elsif @input.last == "exit"
@@ -70,19 +71,24 @@ class WorldRecipes::CLI
       puts ""
       puts "Enter the number of the recipe you'd like to see, cuisines to see cuisine list again, back to return to category list or type exit:"
       @input << gets.strip.downcase
-      i = @input[@input.length - 2]
-      @recipes = @food_categories[i.to_i-1].recipes
+      c = @input[(@input.length)-3]
+      food_categories = @cuisines[c.to_i-1].food_categories
+      fc = @input[@input.length - 2]
+      recipes = food_categories[fc.to_i-1].recipes
 #      binding.pry
-      if @input.last.to_i > 0 && @input.last.to_i <= @recipes.length.to_i
-        @recipes[@input.last.to_i-1].display_recipe
+      if @input.last.to_i > 0 && @input.last.to_i <= recipes.length.to_i
+        recipes[@input.last.to_i-1].display_recipe
         options
       elsif @input.last == "cuisines"
         start
       elsif @input.last == "back"
-        @food_categories.each.with_index(1) do |c, i|
+        food_categories.each.with_index(1) do |c, i|
           puts "#{i}. #{c.name}"
         end
+        @input.pop
+        @input.pop
         category
+        recipe
       elsif @input.last == "exit"
         goodbye
         exit
